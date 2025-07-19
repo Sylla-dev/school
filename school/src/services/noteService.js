e3fb810ae8aa1997eba142741caf245c6f353c69
Sylla-dev <kerfala744@gmail.com>
2025-07-19 02:25:18 +0100

    Update noteService.js

--- school/src/services/noteService.js
+++ school/src/services/noteService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:3000/api/notes";
+const API = "https://school-school-backend.onrender.com";
 
 export const getNotes = () => axios.get(API);
 export const createNote = (data) => axios.post(API, data);
