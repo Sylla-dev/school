cfd0a3a545c76506e787c9df80af0e9fbcddac90
Sylla-dev <kerfala744@gmail.com>
2025-07-19 02:48:47 +0100

    Update teacherService.js

--- school/src/services/teacherService.js
+++ school/src/services/teacherService.js
@@ -3,15 +3,15 @@ import axios from "axios";
 const API_URL = "https://school-school-backend.onrender.com";
 
 // 🔹 Liste des enseignants groupés par spécialité
-export const getTeachers = (spécialité) => axios.get(API_URL, spécialité);
+export const getTeachers = (spécialité) => axios.get(API_URL/enseignants, spécialité);
 
 // 🔹 Liste des enseignants avec leurs matières enseignées
-export const getTeachersWithSubjects = (matieres) => axios.get(API_URL, matieres);
+export const getTeachersWithSubjects = (matieres) => axios.get(API_URL/enseignants, matieres);
 
-export const getTeachersGroupedBySubject = (parmatiere) => axios.get(API_URL, parmatiere);
+export const getTeachersGroupedBySubject = (parmatiere) => axios.get(API_URL/enseignants, parmatiere);
 
 
-export const getTeacherById = (id) => axios.get(`${API_URL}/${id}`);
-export const createTeacher = (data) => axios.post(API_URL, data);
-export const updateTeacher = (id, data) => axios.put(`${API_URL}/${id}`, data);
-export const deleteTeacher = (id) => axios.delete(`${API_URL}/${id}`);
+export const getTeacherById = (id) => axios.get(`${API_URL}/enseignants/${id}`);
+export const createTeacher = (data) => axios.post(API_URL/enseignants, data);
+export const updateTeacher = (id, data) => axios.put(`${API_URL}/enseignants/${id}`, data);
+export const deleteTeacher = (id) => axios.delete(`${API_URL}/enseignants/${id}`);
