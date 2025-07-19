d70ebd3f66bb9c07b3704491192ae553540bf70f
Sylla-dev <kerfala744@gmail.com>
2025-07-19 02:44:49 +0100

    Update matiereService.js

--- school/src/services/matiereService.js
+++ school/src/services/matiereService.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const API_URL = "https://school-school-backend.onrender.com";
 
-export const getMatieres = () => axios.get(API_URL);
-export const getMatiereById = (id) => axios.get(`${API_URL}/${id}`);
-export const createMatiere = (data) => axios.post(API_URL, data);
-export const updateMatiere = (id, data) => axios.put(`${API_URL}/${id}`, data);
-export const deleteMatiere = (id) => axios.delete(`${API_URL}/${id}`);
+export const getMatieres = () => axios.get(API_URL/matieres);
+export const getMatiereById = (id) => axios.get(`${API_URL}/matieres/${id}`);
+export const createMatiere = (data) => axios.post(API_URL/matieres, data);
+export const updateMatiere = (id, data) => axios.put(`${API_URL}/matieres/${id}`, data);
+export const deleteMatiere = (id) => axios.delete(`${API_URL}/matieres/${id}`);
