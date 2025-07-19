0192ec20e91b468b51688687802f7708e72894aa
Sylla-dev <kerfala744@gmail.com>
2025-07-19 02:46:06 +0100

    Update semestreService.js

--- school/src/services/semestreService.js
+++ school/src/services/semestreService.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const API = "https://school-school-backend.onrender.com";
 
-export const getSemestres = () => axios.get(API);
-export const getSemestreById = (id) => axios.get(`${API}/${id}`);
-export const createSemestre = (data) => axios.post(API, data);
-export const updateSemestre = (id, data) => axios.put(`${API}/${id}`, data);
-export const deleteSemestre = (id) => axios.delete(`${API}/${id}`);
+export const getSemestres = () => axios.get(API/notes);
+export const getSemestreById = (id) => axios.get(`${API}/notes/${id}`);
+export const createSemestre = (data) => axios.post(API/notes, data);
+export const updateSemestre = (id, data) => axios.put(`${API}/notes/${id}`, data);
+export const deleteSemestre = (id) => axios.delete(`${API}/notes/${id}`);
